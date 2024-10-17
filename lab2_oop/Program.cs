3ddb458be9e8f9abffb34406460dbe08149aa7bd
Vladyslav Korniichuk <w69943@student.wsiz.edu.pl>
2024-10-17 11:23:51 +0200

    first commit

---
+++ lab2_oop/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+using lab2_oop.Constructions;
+
+Console.WriteLine("Hello World!");
+var construction1 = new Construction();
+var construction2 = new Construction(23.0f, 45.0f, 4, 4, "Material");
+Console.WriteLine("HI");
