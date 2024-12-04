8937f6892c4dc9fd9b0fb7f0da5e4d32b65ff3a9
VladKorniichuk <162552114+VladKorniichuk@users.noreply.github.com>
2024-12-04 21:29:40 +0100

    Add files via upload

---
+++ OOP_lab5/OOP/Constructions/Construction.cs
@@ -0,0 +1,80 @@
+﻿using OOP.Constructions.DTO;
+using OOP.Constructions.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP.Constructions
+{
+    internal class Construction: ISquareCost
+    {
+        public Construction()
+        {
+            Height = 23;
+            Width = 43;
+            Entrances = 3;
+            HumanCapacity = 2;
+            BuildMaterial = BuildMaterialEnum.Concrete;
+        }
+
+        public Construction(CreateConstructionDTO construction)
+        {
+            Height = construction.Height;
+            Width = construction.Width;
+            Entrances = construction.Entrances;
+            HumanCapacity = 2;
+            BuildMaterial = construction.BuildMaterial;
+        }
+
+        public Construction(float height, float width, int entrances, int humanCapacity, BuildMaterialEnum buildMaterial)
+        {
+            Height = height; 
+            Width = width; 
+            Entrances = entrances; 
+            HumanCapacity = humanCapacity; 
+            BuildMaterial = buildMaterial; 
+        }
+
+        public float Height { get; set; }
+        public float Width { get; set; }
+        public int Entrances { get; set; }
+        public int HumanCapacity { get; set; }
+        private BuildMaterialEnum _buildMaterial { get; set; }
+
+
+        public BuildMaterialEnum BuildMaterial { 
+            get 
+            { 
+                return _buildMaterial;
+            } 
+            set
+            {
+                //if (value == null)
+                //{
+                //    throw new ArgumentNullException("Value can not be null");
+                //}
+                _buildMaterial = value;
+            } 
+        }
+
+        public float GetSquareCost()
+        {
+            if (BuildMaterial == BuildMaterialEnum.Concrete)
+            {
+                return 0.87f * Height * Width * 0.9f;
+            }
+            else if (BuildMaterial == BuildMaterialEnum.Brik)
+            {
+                return 0.8f * Height * Width * 0.9f;
+            }
+            return 0.78f * Height * Width * 0.9f;
+        }
+        public double CalculateSquareCost() 
+        {
+            return GetSquareCost();
+        }
+
+    }
+}
---
+++ OOP_lab5/OOP/Program.cs
@@ -0,0 +1,43 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using OOP.BuildingWrapper;
+using OOP.Constructions;
+using OOP.Constructions.DTO;
+using OOP.Constructions.Models;
+using OOP.IndustrialHalls;
+using OOP.ResidentalBuildings;
+
+Console.WriteLine("Hello, World!");
+var construction1 = new Construction();
+var construction2 = new Construction(23.0f, 45.0f, 4, 4, BuildMaterialEnum.Wood);
+var construction3 = new Construction(
+    new CreateConstructionDTO() {
+        BuildMaterial = BuildMaterialEnum.Brik,
+        Width = 100,
+        Height = 100,
+        Entrances = 2
+    }
+);
+Console.WriteLine(construction2.BuildMaterial);
+Console.WriteLine(construction2.Width);
+Console.WriteLine(construction2.HumanCapacity);
+Console.WriteLine(construction2.Height);
+Console.WriteLine(construction2.Entrances);
+
+Console.WriteLine(construction1.BuildMaterial);
+Console.WriteLine(construction1.Width);
+Console.WriteLine(construction1.HumanCapacity);
+Console.WriteLine(construction1.Height);
+Console.WriteLine(construction1.Entrances);
+
+Console.WriteLine(construction1.GetSquareCost());
+var residental = new ResidentalBuilding(3);
+var hall = new IndustrialHall();
+Console.WriteLine(residental.Height);
+Console.WriteLine(hall.Entrances);
+
+var wrapper1 = new BuildingWrapper(residental);
+var wrapper2 = new BuildingWrapper(hall);
+
+Console.WriteLine(wrapper1.GetWrappedSquareCost());
+Console.WriteLine(wrapper2.GetWrappedSquareCost());
